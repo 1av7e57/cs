c0ff6ececd697d803c3887e2018502f2314a4b56
1av7e57 <1av7e57@tutamail.com>
2026-06-16 18:54:41 -0300

    Create Auto.cs

---
+++ Practica04/EjemploSolucion/Estacionamiento/Automotores/Auto.cs
@@ -0,0 +1,36 @@
+﻿namespace Automotores;
+
+using System;
+
+// CLASE PÚBLICA (Requisito para que la vea ConsolaUI)
+public class Auto
+{
+    private string? _marca;
+    private int _modelo;
+
+    // 1. Constructor Vacío
+    public Auto()
+    {
+        _marca = "FIAT";
+        _modelo = DateTime.Now.Year;
+    }
+
+    // 2. Constructor con SOLO marca
+    // Llama al constructor vacío
+    public Auto(string marca) : this() 
+    {
+        _marca = marca; 
+    }
+
+    // 3. Constructor COMPLETO
+    public Auto(string marca, int modelo)
+    {
+        _marca = marca;
+        _modelo = modelo;
+    }
+
+    public string ObtenerDescripcion()
+    {
+        return $"Auto {_marca} {_modelo}";
+    }
+}
