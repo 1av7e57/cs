7c5eccaed9258f2062e5ce80e3a7ff30d3569dc1
1av7e57 <1av7e57@tutamail.com>
2026-08-11 23:42:21 -0300

    Create Program.cs

---
+++ Practica08/Ejemplos/Delegados/Ejemplo02c (anonim)/Program.cs
@@ -0,0 +1,9 @@
+﻿// Definición del namespace que agrupa la lógica de la teoría 8
+using Teoria8;
+
+// Instancia un nuevo objeto de la clase Auxiliar
+Auxiliar aux = new Auxiliar();
+
+// Invoca el método Procesar sobre la instancia creada
+// Esto ejecutará internamente SumaUno(10) y SumaDos(10)
+aux.Procesar();
