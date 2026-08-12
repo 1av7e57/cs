9d26c008930dad4ffd5fcf6282f4a62c557c1255
1av7e57 <1av7e57@tutamail.com>
2026-08-11 22:56:40 -0300

    Add files via upload

---
+++ Practica07/Ejemplos/Teoria07/Empleado.cs
@@ -0,0 +1,9 @@
+namespace Teoria7;
+
+class Empleado : Persona
+{
+    public Empleado(string nombre)
+    => Nombre = nombre;
+    public void Imprimir()
+    => Console.WriteLine($"Soy el empleado {Nombre}");
+}
